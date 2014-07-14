47867871dc7da968d5a7bc3cde564dccd51d5e43
Matteo Sessa <matteo.sessa@catchoftheday.com.au>
2014-07-14 17:27:01 +1000

    remove pyOpenSSL requirement

--- sulphur/misc.py
+++ sulphur/misc.py
@@ -1,8 +1,62 @@
 import re
 import requests
-import base64
 import boto.sns
-import OpenSSL.crypto
+import rsa
+
+from pyasn1.codec.der.decoder import decode
+from pyasn1_modules import rfc2459
+from bitstring import BitArray
+
+def load_pem(contents):
+    def byte_literal(s):
+        return s.encode('latin1')
+
+    pem_start = '-----BEGIN CERTIFICATE-----'
+    pem_end = '-----END CERTIFICATE-----'
+
+    pem_lines = []
+    in_pem_part = False
+
+    for line in contents.splitlines():
+        line = line.strip()
+
+        # Skip empty lines
+        if not line:
+            continue
+
+        # Handle start marker
+        if line == pem_start:
+            if in_pem_part:
+                raise ValueError('Seen start marker "%s" twice' % pem_start)
+
+            in_pem_part = True
+            continue
+
+        # Skip stuff before first marker
+        if not in_pem_part:
+            continue
+
+        # Handle end marker
+        if in_pem_part and line == pem_end:
+            in_pem_part = False
+            break
+
+        # Load fields
+        if byte_literal(':') in line:
+            continue
+
+        pem_lines.append(line)
+
+    # Do some sanity checks
+    if not pem_lines:
+        raise ValueError('No PEM start marker "%s" found' % pem_start)
+
+    if in_pem_part:
+        raise ValueError('No PEM end marker "%s" found' % pem_end)
+
+    # Base64-decode the contents
+    pem = byte_literal('').join(pem_lines)
+    return pem.decode('base64')
 
 def parse_sns_arn(arn):
     regex = re.compile(r"arn:aws:sns:(?P<region>[a-z0-9-]+):(?P<account_id>[0-9]+):(?P<sns_id>.+)")
@@ -34,12 +88,12 @@ def build_signature_string(data_dict):
 def verify_signature(cert_url, signature, data):
     cert = requests.get(cert_url)
     if cert.status_code == 200:
-        x509obj = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert.content)
-        ds = base64.b64decode(signature)
         try:
-            OpenSSL.crypto.verify(x509obj, ds, data, 'sha1')
-            return True
-        except OpenSSL.crypto.Error:
+            decoded_cert = decode(load_pem(cert.content), asn1Spec=rfc2459.Certificate())
+            pubkey_bits = BitArray(list(decoded_cert['tbsCertificate']['subjectPublicKeyInfo']['subjectPublicKey']))
+            rsaobj = rsa.PublicKey.load_pkcs1(pubkey_bits.bytes, format='DER')
+            return rsa.verify(data, signature.decode('base64'), rsaobj)
+        except Exception, e:
             raise
     return False
 
